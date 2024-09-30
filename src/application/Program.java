fb54d874436e5014792fec7d76655688fb8d8d25
Andre Fernandes <dedeh159@gmail.com>
2024-09-30 15:37:04 -0300

    atualização para incluir os comentários

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,13 +12,19 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         Calculadora calc = new Calculadora();
         System.out.println("Digite o valor da compra: ");
+
+        //preenchendo o valor total da compra do cliente
         calc.setTotalDaCompra(sc.nextDouble());
 
         System.out.println("Digite o valor do pagamento do cliente: ");
+
+        //preenchendo o valor do pagamento que o cliente efetuou.
         calc.setValorPagamento(sc.nextDouble());
 
+        //chamando metodo para efetuar calculo do troco
         calc.calculaTroco();
 
+        //impressão do valor em tela para visualização.
         System.out.println(calc.toString());
 
 
--- src/entities/Calculadora.java
+++ src/entities/Calculadora.java
@@ -1,6 +1,8 @@
 package entities;
 
 public class Calculadora {
+
+    //variáveis e vetores utilizados na aplicação
     private double totalDaCompra;
     private double valorPagamento;
     private int[] troco = new int[12];
@@ -8,6 +10,8 @@ public class Calculadora {
     private String[] valoresEscritos = new String[]{"Nota: R$ 100,00", "Nota: R$ 50,00", "Nota: R$ 20,00", "Nota: R$ 10,00",
             "Nota: R$ 5,00", "Nota: R$ 2,00", "Moeda: R$ 1,00", "Moeda: R$ 0,50", "Moeda: R$ 0,25", "Moeda: R$ 0,10", "Moeda: R$ 0,05",
             "Moeda: R$ 0,01"};
+
+    //boolean para chegar se vai haver valor ou não para troco.
     private boolean temTroco = false;
 
     public double getTotalDaCompra() {
@@ -26,14 +30,22 @@ public class Calculadora {
         this.valorPagamento = valorPagamento;
     }
 
-    //100 reais, 50 reais, 20 reais 10 reais, 5 reais, 2 reais, 1 realmoeda, 50 centavos, 25 centavos, 10 centavos
-    //5 centavos, 1 centavo;
+    //metodo que calcula o valor total de troco da operação e gera na variável a o valor para ser calculado
+    //dentro do valor de cada moeda disponivel dentro dos vetores.
     public void calculaTroco() {
         double a = this.valorPagamento - this.totalDaCompra ;
+        //percorrendo todas as moedas salvas dentro do vetor valores
         for (int i = 0; i < 12; i++) {
+            //checando se o valor e superior a cedula e se é maior que zero.
             if (a >= valores[i] && a > 0) {
+
+                //atribuindo a quantidade de cedulas/moedas correspondentes ao troco
                 troco[i] = (int) (a / valores[i]);
+
+                //deixando o resto da subração da moeda para os próximos calculos
                 a =  a - ( troco[i] * valores[i]);
+
+                //alterando para impressão no metodo toString// caso for false ele dirá que não há troco a ser dado
                 this.temTroco = true;
             }
         }
@@ -42,15 +54,20 @@ public class Calculadora {
     @Override
     public String toString() {
 
+            //checando se houve troco na operação calculaTroco
             if (this.temTroco) {
             for (int i = 0; i < 12; i++) {
                 if (troco[i] > 0) {
+                    //efetuando impressão do vetor de quantidade da moeda/cedula e os valores em formato de string
+                    //no vetor valoresEscritos
                     System.out.println(troco[i] + " " + valoresEscritos[i]);
                 }
             }
         }else {
+                //caso não houver troco retornada essa mensagem
                 return "Sem troco para a compra";
             }
+            //impressão do final do calculo
             return "Fim da operação";
     }
 }
